141ba84f12605681c90aa8b27f1d14e4eba3015d
Nick Brooks <nbrooks@thirtyseven.com.au>
2016-12-21 15:37:57 +1030

    Comments are flowin nicely

--- client/components/Comments/CommentNode.js
+++ client/components/Comments/CommentNode.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router';
 import ReactMarkdown from 'react-markdown';
 import Avatar from '../Reusable/Avatar';
 
 const CommentNode = React.createClass({
     render() {
+      const {user, body} = this.props;
+      const userProfileLink = "/u/" + user.id;
+      const username = "@" + user.username;
         return (
           <li>
-            <Avatar user={this.props.user} size="50" customClass="pull-left timeline" />
+
+            <Link to={userProfileLink}><Avatar user={user} size="50" customClass="pull-left timeline" /></Link>
             <div className="ibox comment-body">
-              <ReactMarkdown source={this.props.body} />
+              <Link to={userProfileLink}><h6 className="user">{user.display_name} <span className="username">{username}</span></h6></Link>
+              <hr />
+              <ReactMarkdown source={body} />
             </div>
           </li>
         )
