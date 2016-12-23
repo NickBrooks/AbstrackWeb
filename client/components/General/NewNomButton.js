30a88552055817cdab1b32fb84b81c2815312e4e
Nick Brooks <nbrooks@thirtyseven.com.au>
2016-12-23 19:22:30 +1030

    Button does something

--- client/components/General/NewNomButton.js
+++ client/components/General/NewNomButton.js
@@ -2,10 +2,13 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
 const NewNomButton = React.createClass({
+    openNewNomModal() {
+      alert('this worked');
+    },
     render() {
         return (
           <div className="new-nom-button">
-            <button type="button" className="btn">
+            <button type="button" className="btn" onClick={this.openNewNomModal}>
               <FontAwesome name="pencil" />
             </button>
           </div>
