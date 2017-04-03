7920791b309929f7ced1a7a27958037cdb1ec317
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-04-03 18:57:43 +0930

    Fuck the URL for now

--- client/store/Account/views/ProfileDetailsForm.js
+++ client/store/Account/views/ProfileDetailsForm.js
@@ -26,10 +26,6 @@ class ProfileDetailsForm extends React.Component {
                         <label htmlFor="locationInput"><FontAwesome name="location-arrow" /> Location</label>
                         <input type="text" ref="location" className="form-control" defaultValue={account.location} id="locationInput" placeholder="Share your location" />
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="urlInput"><FontAwesome name="link" /> URL</label>
-                        <input type="url" ref="url" className="form-control" defaultValue={account.url} id="urlInput" placeholder="Enter a url" />
-                    </div>
                     <hr />
                     <div className="form-group">
                         <button type="submit" className="btn btn-success pull-right"><FontAwesome name="caret-right" /> Update profile</button>
