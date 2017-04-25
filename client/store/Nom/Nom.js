9009eb17504cc1e7c57a9e33fab02437d856466e
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-04-25 22:36:32 +0930

    Removed Track/Milestone panels, fix nom source

--- client/store/Nom/Nom.js
+++ client/store/Nom/Nom.js
@@ -11,61 +11,22 @@ class Nom extends React.Component {
         super(props);
     }
 
-    renderMenuItemArray(menuItem) {
-        return [menuItem];
-    }
-
-    renderTrackSubMenu(track) {
-        if (track) {
-            const link = "/t/" + track.id;
-            return (
-                <ul className="nav sub-menu">
-                    <li><h6><FontAwesome name="bookmark" /> Track</h6></li>
-                    <li><Link to={link}>{track.name}</Link></li>
-                </ul>
-            )
-        }
-        return;
-    }
-
-    renderMilestoneSubMenu(milestone) {
-        if (milestone) {
-            const link = "/m/" + milestone.id;
-            return (
-                <ul className="nav sub-menu">
-                    <li><h6><FontAwesome name="calendar-o" /> Milestone</h6></li>
-                    <li><Link to={link}>{milestone.name}</Link></li>
-                </ul>
-            )
-        }
-        return;
-    }
-
     render() {
         const { nomId } = this.props.params;
         const { noms } = this.props;
         const i = noms.findIndex((nom) => nom.id === nomId);
-        const nom = noms[i];
+        const nom = noms[i].data;
         const accountLink = "/u/" + nom.createdBy.id;
         return (
             <div className="row core-body">
-                <div className="col-sm-10">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <Link to={accountLink}><Avatar user={nom.createdBy} size="50" customClass="pull-left timeline" /></Link>
-                            <div className="ibox view-nom">
-                                <NomHeader nom={nom} {...this.props} />
-                                <hr />
-                                <NomBody body={nom.body} />
-                            </div>
-                            <Comments nomId={nom.id} {...this.props} />
-                        </div>
+                <div className="col-sm-12">
+                    <Link to={accountLink}><Avatar user={nom.createdBy} size="50" customClass="pull-left timeline" /></Link>
+                    <div className="ibox view-nom">
+                        <NomHeader nom={nom} {...this.props} />
+                        <hr />
+                        <NomBody body={nom.body} />
                     </div>
-                </div>
-
-                <div className="col-sm-2 sub-bar">
-                    {this.renderTrackSubMenu(nom.track)}
-                    {this.renderMilestoneSubMenu(nom.milestone)}
+                    <Comments nomId={nom.id} {...this.props} />
                 </div>
             </div>
         )
