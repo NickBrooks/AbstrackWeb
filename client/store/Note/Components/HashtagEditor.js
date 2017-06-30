f1ccf869aa0b5160619d6e54bad1ca6c46d6edaa
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-06-30 13:14:21 +0700

    Fixed depracation warnings

--- client/store/Note/Components/HashtagEditor.js
+++ client/store/Note/Components/HashtagEditor.js
@@ -1,13 +1,12 @@
 //libraries
 import React from 'react';
-import Modal from 'react-bootstrap-modal';
 import FontAwesome from 'react-fontawesome';
-import { Typeahead } from 'react-typeahead';
 
 //components
 import HashtagSpan from '../../../components/HashtagSpan/HashtagSpan';
 import { conformHashtags } from '../../../functions';
 
+// TODO: make hashtag editor w/o modal and with new typeahead
 class HashtagEditor extends React.Component {
     constructor(props) {
         super(props);
@@ -56,72 +55,71 @@ class HashtagEditor extends React.Component {
         this.props.removeHashtagsFromNote(hashtags, this.props.noteId);
     }
 
-    renderTypeahead() {
-        let { hashtags, hashtagList } = this.props;
+    // renderTypeahead() {
+    //     let { hashtags, hashtagList } = this.props;
 
-        function removeExistingTags(hashtags, hashtagList) {
-            return hashtags.indexOf(hashtagList.hashtag) === -1;
-        }
+    //     function removeExistingTags(hashtags, hashtagList) {
+    //         return hashtags.indexOf(hashtagList.hashtag) === -1;
+    //     }
 
-        var filteredHashtags = hashtagList.filter(removeExistingTags.bind(null, hashtags));
+    //     var filteredHashtags = hashtagList.filter(removeExistingTags.bind(null, hashtags));
 
-        function displayOption(option) {
-            return "#" + option.hashtag;
-        };
+    //     function displayOption(option) {
+    //         return "#" + option.hashtag;
+    //     };
 
-        return (
-            <div>
-                <Typeahead ref="hashtagTypeahead"
-                    options={filteredHashtags}
-                    filterOption="hashtag"
-                    displayOption={displayOption}
-                    className="hashtag-typeahead"
-                    placeholder="Add #hashtags"
-                    maxVisible={8}
-                    onOptionSelected={this.handleTypeaheadClick}
-                    inputProps={{
-                        autoFocus: true
-                    }}
-                    customClasses={{
-                        input: "form-control"
-                    }}
-                    />
-                <input type="submit" hidden />
-            </div>
-        )
-    }
+    //     return (
+    //         <div>
+    //             <Typeahead ref="hashtagTypeahead"
+    //                 options={filteredHashtags}
+    //                 filterOption="hashtag"
+    //                 displayOption={displayOption}
+    //                 className="hashtag-typeahead"
+    //                 placeholder="Add #hashtags"
+    //                 maxVisible={8}
+    //                 onOptionSelected={this.handleTypeaheadClick}
+    //                 inputProps={{
+    //                     autoFocus: true
+    //                 }}
+    //                 customClasses={{
+    //                     input: "form-control"
+    //                 }}
+    //                 />
+    //             <input type="submit" hidden />
+    //         </div>
+    //     )
+    // }
 
-    renderHashtagEditor() {
-        return (
-            <div>
-                <Modal show={this.state.open} onHide={() => this.setState({ open: false })} aria-labelledby="ModalHeader" className="sm hashtag-editor">
-                    <form ref="hashtagEditor" onSubmit={this.handleHashtagSubmit}>
-                        <Modal.Header>
-                            <Modal.Title id='ModalHeader'>
-                                #hashtag editor
-                            </Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <div>
-                                {this.props.hashtags.map((hashtag, i) => <h5 key={i}>
-                                    <FontAwesome name="times" className="btn-delete-hashtag" onClick={() => this.handleHashtagRemove(hashtag)} /> <span className="tag tag-default hashtag">#{hashtag}</span>
-                                </h5>)}
-                            </div>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            {this.renderTypeahead()}
-                        </Modal.Footer>
-                    </form>
-                </Modal>
-            </div>
-        );
-    }
+    // renderHashtagEditor() {
+    //     return (
+    //         <div>
+    //             <Modal show={this.state.open} onHide={() => this.setState({ open: false })} aria-labelledby="ModalHeader" className="sm hashtag-editor">
+    //                 <form ref="hashtagEditor" onSubmit={this.handleHashtagSubmit}>
+    //                     <Modal.Header>
+    //                         <Modal.Title id='ModalHeader'>
+    //                             #hashtag editor
+    //                         </Modal.Title>
+    //                     </Modal.Header>
+    //                     <Modal.Body>
+    //                         <div>
+    //                             {this.props.hashtags.map((hashtag, i) => <h5 key={i}>
+    //                                 <FontAwesome name="times" className="btn-delete-hashtag" onClick={() => this.handleHashtagRemove(hashtag)} /> <span className="tag tag-default hashtag">#{hashtag}</span>
+    //                             </h5>)}
+    //                         </div>
+    //                     </Modal.Body>
+    //                     <Modal.Footer>
+    //                         {this.renderTypeahead()}
+    //                     </Modal.Footer>
+    //                 </form>
+    //             </Modal>
+    //         </div>
+    //     );
+    // }
 
     render() {
         return (
             <div className="hashtags">
                 {this.props.hashtags.map((hashtag, i) => <HashtagSpan {...this.props} hashtag={hashtag} customClass="default" key={i} i={i} />)}<FontAwesome name="plus" onClick={() => this.setState({ open: true })} />
-                {this.renderHashtagEditor()}
             </div>
         )
     }
